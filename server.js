9e57b8d117496135d05bb69809cb2f25b4129cc0
valiantvu <michelle.thi.vu@gmail.com>
2014-07-07 14:25:39 -0700

    Add server.js

--- server.js
+++ server.js
@@ -1,19 +1,8 @@
 var http = require("http");
-var fs = require("fs");
+var handler = require("./request-handler");
 var port = 8000;
 var ip = "127.0.0.1";
-var server = http.createServer(function(req, res) {
-    
-    headers = {
-      "access-control-allow-origin": "*",
-      "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-      "access-control-allow-headers": "content-type, accept",
-      "access-control-max-age": 10,
-      'Content-Type': "text/html"
-    };
-    res.writeHead(200, headers);
-    res.end(fs.readFileSync('index.html'));
-});
+var server = http.createServer(handler.handleRequest);
 
 console.log("Listening on http://" + ip + ":" + port);
 server.listen(port, ip);
